53bff16bce7e2a36984bdb1e12fa2341eb14a19f
John-Khalil <johnadelkhalil@gmail.com>
2022-06-30 22:13:49 +0200

    dynamic allocation

--- RTosTcpTest/RTOS.cpp
+++ RTosTcpTest/RTOS.cpp
@@ -8,6 +8,46 @@
 #include "spiConsoleSync.cpp"
 
 
+
+unsigned short _LOOP_COUNTER_ = 0;
+unsigned short _NESTED_LOOPS_ = 0;
+#define loop(_LOOP_BODY_,_LOOP_ITERATIONS_) _NESTED_LOOPS_=_LOOP_COUNTER_;_LOOP_COUNTER_=_LOOP_ITERATIONS_;while(_LOOP_COUNTER_--) _LOOP_BODY_;_LOOP_COUNTER_=_NESTED_LOOPS_;
+
+#define lengthInBytes(object) (unsigned char *)(&object+1)-(unsigned char *)(&object)
+
+
+volatile uint32_t *_outputRegisterLow=((volatile uint32_t*)0X3FF44004UL);
+volatile uint32_t *_outputRegisterHigh=((volatile uint32_t*)0X3FF44010UL);
+
+volatile uint32_t *_outputRegisterLowSet=((volatile uint32_t*)0X3FF44008UL);
+volatile uint32_t *_outputRegisterHighSet=((volatile uint32_t*)0X3FF44014UL);
+
+volatile uint32_t *_outputRegisterLowClear=((volatile uint32_t*)0X3FF4400CUL);
+volatile uint32_t *_outputRegisterHighClear=((volatile uint32_t*)0X3FF44018UL);
+
+
+#define outputRegisterLow (*_outputRegisterLow)
+#define outputRegisterHigh (*_outputRegisterHigh)
+#define outputRegisterLowSet (*_outputRegisterLowSet)
+#define outputRegisterHighSet (*_outputRegisterHighSet)
+#define outputRegisterLowClear (*_outputRegisterLowClear)
+#define outputRegisterHighClear (*_outputRegisterHighClear)
+
+#define inputRegisterLow (*((volatile uint32_t*)0x3FF4403CUL))
+#define inputRegisterHigh (*((volatile uint32_t*)0x3FF44040UL))
+
+#define _DW digitalWrite
+#define _DR digitalRead
+#define _PM pinMode
+
+
+
+
+
+
+
+
+
 unsigned char *sha1Hash(unsigned char *rawData){
 	Sha1.init();
 	Sha1.print((char*)rawData);
---
+++ RTosTcpTest/mallocTest.cpp
@@ -0,0 +1,119 @@
+// double colum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class classTest  {
+public:
+	void sayHello(void);
+};
+
+void classTest::sayHello(void) {
+	cout << "this is test \n";
+}
+
+
+classTest ffs;
+
+
+
+
+unsigned short stringCounter(unsigned char *counted) {
+	unsigned short counter = 0;
+	while (*counted) {
+		counter++;
+		counted++;
+	}
+	return counter;
+}
+
+
+unsigned short _LOOP_COUNTER_ = 0;
+unsigned short *_NESTED_LOOPS_= (unsigned short *)malloc(1*sizeof(unsigned short));
+unsigned char _NESTED_LOOPS_COUNTER_ = 0;
+#define loop(_LOOP_ITERATIONS_,_LOOP_BODY_) _NESTED_LOOPS_[_NESTED_LOOPS_COUNTER_++]=_LOOP_COUNTER_; _NESTED_LOOPS_=(unsigned short *)realloc(_NESTED_LOOPS_,(_NESTED_LOOPS_COUNTER_+1) *sizeof(unsigned short));   _LOOP_COUNTER_=_LOOP_ITERATIONS_;while(_LOOP_COUNTER_--) _LOOP_BODY_; _LOOP_COUNTER_=_NESTED_LOOPS_[--_NESTED_LOOPS_COUNTER_];  _NESTED_LOOPS_=(unsigned short *)realloc(_NESTED_LOOPS_, (_NESTED_LOOPS_COUNTER_+1) * sizeof(unsigned short));
+
+#define arrayCounter stringCounter 
+
+unsigned short _FOR_EACH_LOOP_COUNTER = 0;
+#define forEach(_ARRAY_LIKE_STRUCTURE,_SINGLE_ELEMENT_,_FOREACH_LOOP_BODY_) _FOR_EACH_LOOP_COUNTER = 0; loop(arrayCounter(_ARRAY_LIKE_STRUCTURE),{_SINGLE_ELEMENT_=_ARRAY_LIKE_STRUCTURE[_FOR_EACH_LOOP_COUNTER++];_FOREACH_LOOP_BODY_;});
+
+//#define _ARRAY_LIKE_STRUCTURE.forEach((_SINGLE_ELEMENT_){_FOREACH_LOOP_BODY_}) _FOR_EACH_LOOP_COUNTER = 0; loop({_SINGLE_ELEMENT_=_ARRAY_LIKE_STRUCTURE[_FOR_EACH_LOOP_COUNTER++],_FOREACH_LOOP_BODY_;},arrayCounter(_ARRAY_LIKE_STRUCTURE))
+
+int main()
+{/**
+	unsigned char memoryAllocationCounter = 0;
+	unsigned short *p = NULL;
+	p = (unsigned short *)malloc((++memoryAllocationCounter) *sizeof(unsigned short));
+	*p = 20;
+	
+	cout << *p << endl;
+
+	 (unsigned short *)realloc(p, (++memoryAllocationCounter) * sizeof(unsigned short));
+
+	cout << *p << endl;
+
+	unsigned short *testPtr = p + 1;
+	
+	*testPtr = 5;
+
+	cout<< "- "<< p[0] << endl;
+	cout << "-+ " << p[1] << endl;
+	*/
+	
+	int x = 0;
+	loop(3, {
+		loop(3, {
+			loop(3, {
+				loop(3, {
+					loop(3, {
+						cout << ++x << endl;
+					});
+				});
+			});
+		});
+	});
+
+	/**
+	unsigned char *testPtr = NULL;
+	testPtr = new unsigned char[18];
+
+
+
+
+	cout << " test to if mem allocation worked >>>>          "<< sizeof(testPtr) << endl;
+
+	unsigned char testArray[10] = "hello!";
+
+	unsigned char *testTestPtr = testPtr + 3;
+
+	free(testTestPtr);
+	
+	memcpy(testPtr, testArray,sizeof(testArray));
+
+
+	unsigned char variable;
+	forEach(testPtr, variable, {
+
+
+		cout<< "- -" << variable <<endl;
+	});
+	
+	//ffs.sayHello();
+
+    cout << "\nHello World!\n";
+
+	*/
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
