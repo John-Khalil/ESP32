64596f34f1025f93819e15fcd56a181009b530ae
John-Khalil <johnadelkhalil@gmail.com>
2022-10-04 21:40:06 +0200

    i hate css :)

--- executable/service client/service-client/src/components/ConsoleDynamic.js
+++ executable/service client/service-client/src/components/ConsoleDynamic.js
@@ -10,24 +10,56 @@ const DynamicConsole=({userConsole})=>{
 
 	return(
 		<>
-			<div className={`w-[calc(100% -24)] h-[320px] p-1 m-1 overflow-scroll rounded-lg bg-gray-900`} style={{
+			<div className={`p-1 m-1 overflow-scroll rounded-lg bg-gray-900`} style={{
 				border:`1px solid ${userConsole.themeColor||defaultThemeColor}`,
 				color:userConsole.textColor||defaultTextColor
 			}}>
-				<div className={`w-[calc(100% -32)] p-1 m-1 overflow-scroll rounded-lg `} style={{
-					border:`1px solid ${userConsole.themeColor||defaultThemeColor}`,
-					color:userConsole.textColor||defaultTextColor
-				}}>
-					<input type="text" className="w-[calc(99.5%)] p-1 m-1 rounded-lg bg-gray-700" style={{
+				
+
+				<>
+					{(consoleOutput)?(
+						<div className="w-[calc(100% -24)] h-[320px] m-1 p-1">
+							ffs
+							
+						</div>
+					):(<></>)}
+				</>
+				
+				{(consoleInput)?(
+				
+					<div className={`w-[calc(100% -24)] p-1 m-1 overflow-scroll rounded-lg bg-black`} style={{
 						border:`1px solid ${userConsole.themeColor||defaultThemeColor}`,
 						color:userConsole.textColor||defaultTextColor
-					}}/>
-					<div className="float-rigth">
-					<input type="checkbox" className="inline-flex float-right m-1 mr-3 p-1"/>
-						<span className="inline-flex float-right mr-1 ">NL</span>
+					}}>
+
+						<input type="text" className="w-[calc(75%)] p-1 m-1 rounded-lg bg-white mt-3" style={{
+							border:`1px solid ${userConsole.themeColor||defaultThemeColor}`,
+							color:userConsole.textColor||defaultTextColor
+						}}/>
+
 						
+						
+
+						<div className="float-right p-1 block">
+							<div className="block">
+								<input type="checkbox" className="inline-block float-right m-1 mt-1.5 mr-1 p-1"/>
+								<span className="inline-block float-right mr-1 text-white">NL</span>
+							</div>
+							<div className="block">
+								<input type="checkbox" className="inline-block float-right m-1 mt-1.5 mr-1 p-1"/>
+								<span className="inline-block float-right mr-1 text-white">CR</span>
+							</div>
+							
+						</div>
+
+						<button className={`m-1 p-1 pl-2 pr-2 inline rounded-lg float-right mt-3`} style={{
+							border:`1px solid ${userConsole.themeColor||defaultThemeColor}`,
+							color:"white",
+							background:userConsole.textColor||defaultTextColor
+						}}>Send</button>
 					</div>
-				</div>
+				
+				):(<></>)}
 
 				
 			</div>
