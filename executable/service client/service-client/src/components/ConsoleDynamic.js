55916fb7f06fdbf40ce89d7ea24f63941cfc1cd8
John-Khalil <johnadelkhalil@gmail.com>
2022-10-04 07:55:32 +0200

    ...

--- executable/service client/service-client/src/components/ConsoleDynamic.js
+++ executable/service client/service-client/src/components/ConsoleDynamic.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React,{ useState,useEffect } from "react"
+
+const DynamicConsole=()=>{
+	return(
+		<>
+			
+		</>
+	);
+}
 
 export default function ConsoleDynamic() {
   return (
--- executable/service client/service-client/src/layouts/Layout.js
+++ executable/service client/service-client/src/layouts/Layout.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import {Link,Outlet} from 'react-router-dom'
 
 export default function Layout() {
 	const navBarStyleElements='felx p-1 text-center inline-block border border-sky-800 hover:bg-sky-600 w-18 font-bold';
+	const navBarHeight=67;
+	const [windowHeight,setWindowHeight]=useState((window.innerHeight-navBarHeight))
+	useEffect(()=>{
+		window.addEventListener("resize",()=>{
+			setWindowHeight(window.innerHeight-navBarHeight);
+		},false);
+	},[])
   return (
     <>
 			<nav className='m-1 p-1 text-right border border-sky-800 '>
@@ -11,7 +18,7 @@ export default function Layout() {
 				<Link to="/app-settings"> <div className={navBarStyleElements}>Settings</div> </Link>
 			</nav>
 			<div className='m-1 p-1 text center border border-sky-800'>
-				<div className='w-full h-screen'>
+				<div className='w-full divHeight' style={{'--height':`${windowHeight}px`}}>
 					<Outlet/>
 				</div>
 			</div>
