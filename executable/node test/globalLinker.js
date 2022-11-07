1acbc56e102e5ab5514e1824c8cb522c29753ab2
John-Khalil <johnadelkhalil@gmail.com>
2022-11-07 13:04:10 +0200

    queuing system js

--- executable/node test/globalLinker.js
+++ executable/node test/globalLinker.js
@@ -2,21 +2,65 @@ import axios from "axios";
 import WebSocket from "ws";
 
 // --es-module-specifier-resolution=node
+
+
+
+
+/*
+	^ WRITE SIDE 
+		* get the data 
+		* wait for the SYNC REGISTER to update to the last sent sequence
+		* wrape it inside the object
+		* encode it
+		* pass it to the threads
+	^ READ SIDE
+		* decode the data
+		* check for packet type
+		? if normal packet 
+			* wrape it inside feedback object
+			* send the feedback object
+			* pass the packet to threads
+		? if feedback packet 
+			* update the SYNC REGISTER
+
+*/
+
+
+
 export default class globalLinker{
     readCallbackList=[];            //^ read data from realtime connection -- this would later be set by the user
     writeCallbackList=[];           //^ send the data through real time connection
+
+    syncRegister=0;
+    packetSequence=0;
+    DEV_ID=255;
+
+    encode(txData){
+        return txData;
+    }
+
+    decode(rxData){
+        return rxData;
+    }
     
+    async linkerSend(dataToList,devId=this.DEV_ID){
+
+
+        devId=devId<<24;
+        if(!syncRegister)
+            syncRegister=devId;
+
+        this.writeCallbackList.forEach(callBackFunction => {
+            callBackFunction(dataToList);
+        });
+    }
+
     async linkerSet(dataToList){
         this.readCallbackList.forEach(callBackFunction => {
             callBackFunction(dataToList);
         });
     }
 
-    async linkerSend(dataToList){
-        this.writeCallbackList.forEach(callBackFunction => {
-            callBackFunction(dataToList);
-        });
-    }
 
     async linkerSetAdd(callBack){
         this.readCallbackList.push(callBack);
