6e81c5781788062712850997a7245bb5becee8c8
John-Khalil <johnadelkhalil@gmail.com>
2023-05-17 02:37:59 +0300

    led matrix

--- executable/service client/service-client/src/components/HomePage.js
+++ executable/service client/service-client/src/components/HomePage.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export default function HomePage() {
+  const distanceSensorRef=useRef(null);
+
+  const displayTextRef=useRef(null);
+
+  const socket = new WebSocket(`ws://192.168.1.11:80/`);
+
+  socket.addEventListener('open',()=>{
+    console.log("CONNECTED");
+  });
+
+  socket.addEventListener('message',(event)=>{
+    distanceSensorRef.current.innerHTML=`Distance ${JSON.parse(event.data).distance} CM`;
+  });
+
+  socket.addEventListener('close',()=>{
+
+  });
+
+  socket.addEventListener('error',()=>{
+
+  });
+
+
   return (
-    <div>HomePage</div>
+    <>
+      <input className='rounded-md p-1 m-1 text-black' type="text" ref={displayTextRef}/>
+      <br />
+
+      <button className='bg-green-500 rounded-md p-1 m-1' onClick={()=>{
+        socket.send(JSON.stringify({
+          msg:displayTextRef.current.value
+        }));
+      }}>SUBMIT</button>
+
+      <div ref={distanceSensorRef}>
+
+      </div>
+    </>
   )
 }
