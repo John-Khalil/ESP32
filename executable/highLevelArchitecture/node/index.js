cc96814a7f41dda18630bf9e17882b80ca97255e
John-Khalil <johnadelkhalil@gmail.com>
2023-09-02 08:42:15 +0300

    express app setup

--- executable/highLevelArchitecture/node/index.js
+++ executable/highLevelArchitecture/node/index.js
@@ -1,12 +1,38 @@
+import express  from 'express';
+import cors from 'cors';
+import util from 'util';
+import bodyParser from 'body-parser';
+
 import mqttClient from "./mqttClient.js";
 
 console.clear();
 
 const remoteClient=new mqttClient({serverAddress:'mqtt-dashboard.com',topic:mqttClient.MQTT_TOPIC,startup:()=>{
-    remoteClient.send("test node server");
+    // remoteClient.send("test node server");
     remoteClient.onData(data=>{
         console.log(data);
-    })
+    });
 }});
+
+const app =express();
+app.use(cors());
+app.use(bodyParser.json());
+const port =766;
+app.listen(port,()=>{
+    console.log(`-------- server started @ port ${port}`);
+});
+
+app.get('/',(req,res)=>{
+
+    remoteClient.send("test node server");
+    res.send('ack');
+})
+
+app.post('/upload',(req,res)=>{
+    res.send('ack');
+    remoteClient.send("test node server");
+
+    console.log(JSON.stringify(req.body))
+});
 
 
