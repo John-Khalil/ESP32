3d04fa5c257f9ee37186bd715c59ee4269e140bf
John-Khalil <johnadelkhalil@gmail.com>
2024-10-03 01:09:37 +0300

    stepper class

---
+++ executable/highLevelArchitecture/custom/stepper.cpp
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#include <stdint.h>
+#include <functional>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <string>
+#include <type_traits>
+
+class stepper{
+	stepper(){
+
+	}
+	~stepper(){
+		
+	}
+};
