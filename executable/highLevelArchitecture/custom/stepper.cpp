c8ea6b0700c908080718a7c184c15fbab92ef4e4
John-Khalil <johnadelkhalil@gmail.com>
2024-10-05 04:49:22 +0300

    stepper class

--- executable/highLevelArchitecture/custom/stepper.cpp
+++ executable/highLevelArchitecture/custom/stepper.cpp
@@ -12,7 +12,7 @@
 class stepper{
 
 	int64_t refPoint=0;
-	int64_t currentPoint=0;
+	volatile int64_t currentPoint=0;
 
 	std::function<void(void)> checkPosition=nullptr;
 
@@ -27,7 +27,7 @@ class stepper{
 
 	void outputPort(uint32_t value){
 		if(externalPort!=nullptr){
-			outputPort(value);
+			externalPort(value);
 			return;
 		}
 		uint32_t registerValue=*outputRegister;
@@ -36,7 +36,51 @@ class stepper{
 		*outputRegister=registerValue;
 	}
 
-	
+	#define OUTPUT_PORT(value) do { \
+		if (externalPort != nullptr) { \
+			externalPort(value); \
+			return; \
+		} \
+		uint32_t registerValue = *outputRegister; \
+		registerValue &= ~((1 << dir) | (1 << step) | (1 << en)); \
+		registerValue |= (value); \
+		*outputRegister = registerValue; \
+	} while(0)
+
+
+	void move(void){
+		portValue|=(1<<en);
+		if(checkPosition==nullptr){
+			while(refPoint!=currentPoint){
+				if(refPoint>currentPoint){
+					portValue|=(1<<dir);
+					outputPort(portValue^=(1<<step));
+					currentPoint++;
+				}
+				else if(refPoint<currentPoint){
+					portValue&=~(1<<dir);
+					outputPort(portValue^=(1<<step));
+					currentPoint--;
+				}
+			}
+		}
+		else{
+			while(refPoint!=currentPoint){
+				if(refPoint>currentPoint){
+					portValue|=(1<<dir);
+					outputPort(portValue^=(1<<step));
+					checkPosition();
+					currentPoint++;
+				}
+				else if(refPoint<currentPoint){
+					portValue&=~(1<<dir);
+					outputPort(portValue^=(1<<step));
+					checkPosition();
+					currentPoint--;
+				}
+			}
+		}
+	}
 
 
 
