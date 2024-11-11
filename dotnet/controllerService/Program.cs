c5dbb11dbe25042c83c61c0faf73259aff38f2a8
John Khalil <104776571+John-Khalil@users.noreply.github.com>
2024-11-11 16:15:32 +0200

    controllerService

---
+++ dotnet/controllerService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Ports;
+
+class Program
+{
+    static void Main()
+    {
+        SerialPort serialPort = new SerialPort("COM14", 9600, Parity.None, 8, StopBits.One);
+
+        try
+        {
+            serialPort.Open();
+            Console.WriteLine("Serial Port Opened");
+
+            // Write to the serial port
+            serialPort.WriteLine("Hello, Serial Port!");
+
+            // Read from the serial port
+            string response = serialPort.ReadLine();
+            Console.WriteLine("Received: " + response);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                Console.WriteLine("Serial Port Closed");
+            }
+        }
+    }
+}
