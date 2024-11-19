ba83953d3ea13667282011c848f07d44818731cd
John Khalil <104776571+John-Khalil@users.noreply.github.com>
2024-11-19 04:17:48 +0200

    ..

--- dotnet/controllerService/Program.cs
+++ dotnet/controllerService/Program.cs
@@ -24,14 +24,3 @@ class Program
         while (Console.ReadKey().Key != ConsoleKey.Enter);
     }
 }
-
-// string json = @"{ ""Name"": ""Alice"", ""Age"": 30, ""City"": ""Wonderland"" }";
-
-// // Parse JSON into a JObject
-// JObject jObject = JObject.Parse(json);
-
-// // Loop through each property in the JObject
-// foreach (var property in jObject.Properties())
-// {
-//     Console.WriteLine($"Key: {property.Name}, Value: {property.Value}");
-// }
