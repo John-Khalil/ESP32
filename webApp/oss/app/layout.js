3c2a961947a4915d061bc6689d8bee3910aa58dd
John-Khalil <johnadelkhalil@gmail.com>
2024-12-22 03:58:31 +0200

    ..

--- webApp/oss/app/layout.js
+++ webApp/oss/app/layout.js
@@ -1,17 +1,41 @@
-import "./globals.css";
+'use client';
+// import "./globals.css";
 import '../public/css/main.css';
+import { ThemeProvider,createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
-export const metadata = {
-  title: "oss",
-  description: "omni smart solution",
-};
+// export const metadata = (()=>{
+//   'use server';
+//   return({
+//     title: "oss",
+//     description: "omni smart solution",
+//   });
+// })();
+
 
 export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
       <body className={``}>
-        {children}
+        <ThemeProvider theme={createTheme({
+          // palette: {
+          //   mode: 'dark',
+          //   primary: {
+          //     main: '#90caf9',  // A modern blue shade
+          //   },
+          //   secondary: {
+          //     main: '#a5d6a7',  // A soft green shade
+          //   },
+          //   background: {
+          //     default: '#121212',  // Material Design recommended dark background
+          //     paper: '#1e1e1e',    // Slightly lighter for cards/surfaces
+          //   },
+          // }
+        })}>
+          <CssBaseline/>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- webApp/oss/components/NavBar.jsx
+++ webApp/oss/components/NavBar.jsx
@@ -1,8 +1,9 @@
 // 'use client'
 import React from 'react'
 import Tbot from '@/assets/Tbot'
+import { Stack,Box,Paper } from '@mui/material'
 
-export default function NavBar(props) {
+export  function NavBar2(props) {
   return (
     <nav className='appNavBar'>
 			<div className='navItems'>
@@ -30,5 +31,44 @@ export default function NavBar(props) {
 				)}
 			</div>
     </nav>
+  )
+}
+
+export default function NavBar(props){
+  return (
+    <Stack direction="row" spacing={2}>
+			<Box sx={{
+				display:'flex',
+				overflowX:'auto',
+				position:'nowrap',
+			}}>
+				{Array.from(Array(props?.navList?.length)).map((__,index)=>
+					<Stack key={`appNavBar - ${index}`} spacing={2} sx={{
+						// display:'flex',
+						// alignItems:'center'
+					}}>
+						<Paper elevation={3}>
+							<Stack>
+								<Box display="flex" justifyContent="center"
+    alignItems="center" sx={{
+									position:'absolute',
+									paddingTop:'100px',
+									width:'max-content',
+									// alignItems:'center'
+								}}>
+									{props?.navList[index]?.title}
+								</Box>
+								{props?.navList[index]?.icon}
+							</Stack>
+						</Paper>
+					</Stack>
+				)}
+			</Box>
+			<Box sx={{
+				
+			}}>
+
+			</Box>
+		</Stack>
   )
 }
